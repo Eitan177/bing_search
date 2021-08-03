1d75cd746a3d299e4fc70f185438bdb1460ff094
Eitan Halper-Stromberg <Eitan177@gmail.com>
2021-08-03 12:58:40 -0400

    Add files via upload

--- search.py
+++ search.py
@@ -1,16 +1,19 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(layout="wide")
-st.title('similar image search with sp unknowns and microsoft')
+st.title('similar image search with sp unknowns')
 
 col1, col2= st.beta_columns(2)
 
 with col1:
-    video_file = open('streamlit-search-2021-08-03-10-08-47.webm', 'rb')
+    video_file = open('streamlit-search-2021-08-03-10-08-47.mp4', 'rb')
     video_bytes = video_file.read()
     st.video(video_bytes)
-col1, col2 = st.beta_columns(2)
 
+with col2:
+    st.title('<-- Tutorial Video is on the left. The search, including the visual search box and the digital slides, is immediately below the video (scroll down to see)')
+
+col1, col2 = st.beta_columns(2)
 
 
 with col1:
