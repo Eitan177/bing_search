be6b2f2323b532b4b7820b4a29b69d05f4d5eea0
Eitan Halper-Stromberg <Eitan177@gmail.com>
2021-08-11 14:16:05 -0400

    Update search.py

--- search.py
+++ search.py
@@ -2,6 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 import os
 import pyimgur
+import pdb
 
 
 st.set_page_config(layout="wide")
@@ -28,14 +29,20 @@ class similar:
             st.session_state['bingkey'] = 0
         if 'googlekey' not in st.session_state:
             st.session_state['googlekey']=0
+        if 'alkey' not in st.session_state:
+            st.session_state['alkey']=0    
         bing_key=st.session_state['bingkey']
         google_key=st.session_state['googlekey']
+        album_key=st.session_state['alkey']
         while bing_key>=0:   
             self.showbing(bing_key)
             bing_key-=1
         while google_key>=0:
             self.showgoog(google_key)
             google_key-=1
+        while album_key>=0:
+            self.showalbum(album_key)
+            album_key-=1
 
     def showbing(self,key):
         col1, col2 = st.beta_columns(2)
@@ -57,6 +64,7 @@ class similar:
                 200, 2000,900, 100)
                 components.iframe("https://digital.pathology.johnshopkins.edu/imageSets/451", width=wuse2,height=huse2)
         if key==0:
+            st.title('Click Button Below to Add another row for searching bing')
             bing_row_add=st.button('add bing visual search row '+str(st.session_state['bingkey'])+' (DOUBLE CLICK THIS BUTTON)')
             if bing_row_add:
                 st.session_state['bingkey']+=1
@@ -103,10 +111,36 @@ class similar:
                     st.header("https://www.google.com/searchbyimage?image_url="+path+"&q="+words1+words2+words3+restrictpage)
             
         if key==0: 
+            st.title('Click Button Below to Add another row for searching google')
             goog_row_add=st.button('add google visual search row' +str(st.session_state['googlekey'])+ ' (DOUBLE CLICK THIS BUTTON)')
             if goog_row_add:
                 st.session_state['googlekey']+=1
                                  
+        return self
+    def showalbum(self,key):
+        col1, col2 = st.beta_columns(2)
+        with col1:
+                wuse3=st.slider(
+                'left albumizr window width for row '+str(key),
+                200, 2000,900, 100)
+                huse3=st.slider(
+                'left albumizr window height for row '+str(key),
+                200, 2000,900, 100)
+                components.iframe("https://albumizr.com/a/1iDK",width=wuse3, height=huse3)
+
+        with col2:
+                wuse4=st.slider(
+                'right albumizr window width for row '+str(key),
+                200, 2000,900, 100)
+                huse4=st.slider(
+                'right albumizr window height for row '+str(key),
+                200, 2000,900, 100)
+                components.iframe("https://albumizr.com/", width=wuse4,height=huse4)
+        if key==0:
+            st.title('Click Button Below to Add another row for Viewing Social Media')
+            album_row_add=st.button('add albumizr row '+str(st.session_state['alkey'])+' (DOUBLE CLICK THIS BUTTON)')
+            if album_row_add:
+                st.session_state['alkey']+=1
         return self
      
 showsim=similar()
